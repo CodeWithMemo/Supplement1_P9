4e9b0797cfbfe71f4a29911b869e9530ea7535c1
Guillermo <Antonioflores04@icloud.com>
2025-02-27 12:08:12 -0500

    Add unit test for RandomFloatGenerator exception handling

--- Supplement1_P9.Tests/UnitTest1.cs
+++ Supplement1_P9.Tests/UnitTest1.cs
@@ -3,8 +3,22 @@ namespace Supplement1_P9.Tests;
 public class UnitTest1
 {
     [Fact]
-    public void Test1()
+    public void RandomFloatGeneratorShouldThrowInvalidSequenceException()
     {
+        var generator = new RandomFloatGenerator();
+        var enumerator = generator.GetEnumerator();
+        
+        try
+        {
+            for (int i = 0; i < 1000; i++)
+                enumerator.MoveNext();
+        }
+        catch (InvalidSequenceException)
+        {
+            Assert.True(true);
+            return;
+        }
 
+        Assert.False(true, "Expected InvalidSequenceException to be thrown.");
     }
 }
