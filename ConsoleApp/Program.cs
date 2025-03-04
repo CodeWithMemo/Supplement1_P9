4765572460f384d199c68be4ecad602420b938a0
Guillermo <Antonioflores04@icloud.com>
2025-03-04 15:01:04 -0500

    Initialize list of quarters and set up random float generator with enumerator

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -6,6 +6,9 @@ namespace Supplement1_P10
 {
     class Program{
         static void main{
+            List<Quarter> quarters = new List<Quarter>();
+            RandomFloatGenerator generator = new RandomFloatGenerator();
+            var enumerator = generator.GetEnumerator();
             
         }
     }
